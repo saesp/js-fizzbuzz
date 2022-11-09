b2a88f0535b763f3c31ab24c39281cc61ce6d5bc
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-09 15:38:32 +0100

    Completed logic

--- logic.js
+++ logic.js
@@ -13,15 +13,17 @@ const container = document.querySelector("div.container");
 
 for (let i = 0; i < 101; i++){
     const element = `<div class="box box-${i}">${i}</div>`;
-    container.innerHTML += element;
+    const element3 = `<div class="box box-${i % 3 === 0}">Fizz</div>`;
+    const element5 = `<div class="box box-${i % 5 === 0}">Buzz</div>`;
+    const element35 = `<div class="box box-${i % 3 === 0 && i % 5 === 0};">FizzBuzz</div>`;
 
     if (i % 3 === 0) {
-        element 
+        container.innerHTML += element3;
     } else if (i % 5 === 0) {
-
+        container.innerHTML += element5;
     } else if (i % 3 === 0 && i % 5 === 0) {
-
-    } else () {
-
+        container.innerHTML += element35;
+    } else {
+        container.innerHTML += element;
     }
 }
