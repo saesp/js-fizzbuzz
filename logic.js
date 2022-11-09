46b1ca5855e0a43c2707ab3a842132187c925a5f
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-09 16:14:54 +0100

    completed logic

--- logic.js
+++ logic.js
@@ -12,26 +12,26 @@
 const container = document.querySelector("div.container");
 
 for (let i = 0; i < 101; i++){
-    console.log(i);
     const element = `<div class="box box-${i}">${i}</div>`;
     const element3 = `<div class="box box-${i % 3 === 0}">Fizz</div>`;
     const element5 = `<div class="box box-${i % 5 === 0}">Buzz</div>`;
     const element35 = `<div class="box box-${i % 3 === 0 && i % 5 === 0};">FizzBuzz</div>`;
 
-    if (i % 3 === 0) {
-        container.innerHTML += element3;
-        const element3Sec = "Fizz";
-        console.log(element3Sec);
+    if (i % 3 === 0 && i % 5 === 0) {
+        container.innerHTML += element35;
+        const element35Sec = "FizzBuzz";
+        console.log(element35Sec);
     } else if (i % 5 === 0) {
         container.innerHTML += element5;
         const element5Sec = "Buzz";
         console.log(element5Sec);
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        container.innerHTML += element35;
-        const element35Sec = "FizzBuzz";
-        console.log(element35Sec);
+    } else if (i % 3 === 0) {
+        container.innerHTML += element3;
+        const element3Sec = "Fizz";
+        console.log(element3Sec);
     } else {
         container.innerHTML += element;
-        console.log(element);
+        const elementSec = i;
+        console.log(elementSec);
     }
 }
